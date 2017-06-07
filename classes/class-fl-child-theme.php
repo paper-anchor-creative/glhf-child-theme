5059e2f70ab09df1457d578d271e35c5905343c5
Nic Ford <nicdford@users.noreply.github.com>
2017-06-06 20:19:52 -0700

    Update class-fl-child-theme.php

--- classes/class-fl-child-theme.php
+++ classes/class-fl-child-theme.php
@@ -15,5 +15,6 @@ final class FLChildTheme {
     static public function enqueue_scripts()
     {
 	    wp_enqueue_style( 'fl-child-theme', FL_CHILD_THEME_URL . '/style.css' );
+	    wp_enqueue_style( 'glhf-style', get_stylesheet_directory_uri() .'/css/style.css', array('fl-child-theme'), '1.0.0', false);
     }
 }
